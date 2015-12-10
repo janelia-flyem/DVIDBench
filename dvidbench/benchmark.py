d5158f38ee1a054baf0fc25534020e141c2a58b9
Jody Clements <jody@neomorphic.co.uk>
2015-12-10 10:52:41 -0500

    Fixed some log processing bugs

--- dvidbench/benchmark.py
+++ dvidbench/benchmark.py
@@ -1,12 +1,8 @@
-#! /usr/bin/env python
+class Benchmark:
 
-import json
+    foo = 'bar'
 
-
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description='Benchmark a DVID server')
-    parser.add_argument('--server', required=True, help='DVID server url')
-    cl_args = vars(parser.parse_args())
-
+    def __init__(self):
+        self.config = {}
+        return
 
